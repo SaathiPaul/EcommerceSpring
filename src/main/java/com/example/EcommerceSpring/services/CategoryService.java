97fec7aebbfd3f5d2ebf15662d6826ed1f3fa59b
Saathi Paul <saathipaul@gmail.com>
2025-10-17 22:14:59 +0530

    add Category Service

---
+++ src/main/java/com/example/EcommerceSpring/services/CategoryService.java
@@ -0,0 +1,45 @@
+package com.example.EcommerceSpring.services;
+
+import com.example.EcommerceSpring.dto.CategoryDTO;
+import com.example.EcommerceSpring.entity.Category;
+import com.example.EcommerceSpring.mappers.CategoryMapper;
+import com.example.EcommerceSpring.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryService implements ICategoryService{
+
+    private final CategoryRepository repo;
+
+    public CategoryService(CategoryRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<CategoryDTO> getAllCategories() throws IOException {
+        List<CategoryDTO> list= new ArrayList<>();
+        for(Category category : repo.findAll())
+        {
+            list.add(CategoryMapper.toDto(category));
+        }
+        return list;
+    }
+
+    public CategoryDTO createCategory(CategoryDTO categoryDTO)
+    {
+        Category category = CategoryMapper.toEntity(categoryDTO);
+        Category saved = repo.save(category);
+        return CategoryMapper.toDto(category);
+    }
+
+   public CategoryDTO getByName(String name) throws Exception
+   {
+       Category category = repo.findByName(name)
+               .orElseThrow(() -> new Exception("Category Not found"));
+       return CategoryMapper.toDto(category);
+   }
+
+}
